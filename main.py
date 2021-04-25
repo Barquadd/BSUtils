544420e620621feb0c971121fe9127988e3a326b
Barquadd <bm4qdLOP@gmail.com>
2021-04-24 23:20:01 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import subprocess
+import os
+import psutil
+
+def aswDisable():
+    #There is 100% a better way to do this but I'm not good at programming
+    with open('aswDisable.bat', 'w') as aswDisableScript:
+        aswDisableScript.write('echo server:asw.Off > "aswDisableCmds.txt"\necho exit >> "aswDisableCmds.txt"\n' + r'call "C:\Program Files\Oculus\Support\oculus-diagnostics\OculusDebugToolCLI.exe" -f "%~dp0\aswDisableCmds.txt"' + '\ndel "aswDisableCmds.txt"')
+    subprocess.call(['aswDisable.bat'])
+    os.remove('aswDisable.bat')
+    aswDisableBtn.setEnabled(False)
+
+def changeBSPriority():
+    BSOpen = False
+    for proc in psutil.process_iter():
+        if proc.name() == 'BeatSaber.exe':
+            BSOpen = True
+            psutil.Process(proc.pid).nice(psutil.HIGH_PRIORITY_CLASS)
+    if BSOpen != True:
+        alertBox = QtWidgets.QMessageBox()
+        alertBox.setWindowTitle("BSUtils")
+        alertBox.setText("Priority Could Not Be Changed, Is Beat Saber Open?")
+        alertBox.exec_()
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.setEnabled(True)
+        #I doubt this application is gonna handle 4k monitors well, but I doubt many people will use this anyway.
+        Dialog.resize(400, 300)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(20, 20, 81, 31))
+        font = QtGui.QFont()
+        font.setBold(False)
+        font.setWeight(50)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(aswDisable)
+        global aswDisableBtn
+        aswDisableBtn = self.pushButton
+        self.changePriority = QtWidgets.QPushButton(Dialog)
+        self.changePriority.setGeometry(QtCore.QRect(140, 20, 111, 31))
+        font = QtGui.QFont()
+        font.setBold(False)
+        font.setWeight(50)
+        self.changePriority.setFont(font)
+        self.changePriority.setObjectName("changePriority")
+        self.changePriority.clicked.connect(changeBSPriority)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "BSUtils"))
+        self.pushButton.setText(_translate("Dialog", "Disable ASW"))
+        self.changePriority.setText(_translate("Dialog", "Change BS Priroity"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
